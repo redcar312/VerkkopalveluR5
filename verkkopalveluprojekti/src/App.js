db4419e68de1989e0b80b80a9bd8fda0cd0351af
Joonas Kelahaara <jolle.kela@gmail.com>
2021-11-04 17:42:24 +0200

    bootstrap testi

--- verkkopalveluprojekti/src/App.js
+++ verkkopalveluprojekti/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {useEffect, useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap';
 import axios from 'axios';
 
 function App() {
   return (
     <div className="container">
-      <div className="col-12">
+      <div className="col-6">
         <h3>Etusivu</h3>
+        <button className="btn btn-primary">Test</button>
       </div>
     </div>
   );
