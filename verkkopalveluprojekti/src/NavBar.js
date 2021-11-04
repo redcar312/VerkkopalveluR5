5e5bc6c200d936c857814c8a2e4886a88c01a90d
Joonas Kelahaara <jolle.kela@gmail.com>
2021-11-04 18:19:34 +0200

    navbarin valmistelua

--- verkkopalveluprojekti/src/NavBar.js
+++ verkkopalveluprojekti/src/NavBar.js
@@ -7,12 +7,12 @@ export default function NavBar() {
   <div class="container-fluid">
     <Link class="navbar-brand" to="/">Navbar</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
-    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+        <Link class="nav-link" to="/">Home</Link>
         <Link class="nav-link" to="/">Features</Link>
         <Link class="nav-link" to="/">Pricing</Link>
       </div>
