03f5754465343dab4d42d6ba5a616e39e65dbb78
Joonas Kelahaara <jolle.kela@gmail.com>
2021-11-05 17:40:35 +0200

    tyylittelyn aloitusta

--- verkkopalveluprojekti/src/NavBar.js
+++ verkkopalveluprojekti/src/NavBar.js
@@ -1,23 +1,73 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function NavBar() {
-    return(
-  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div className="container-fluid">
-      <Link className="navbar-brand" to="/">Navbar</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" 
-        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <Link className="nav-link" to="/">Home</Link>
-          <Link className="nav-link" to="/">Features</Link>
-          <Link className="nav-link" to="/">Pricing</Link>
+export default function NavBar () {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+      <div className='container-fluid'>
+        <Link className='navbar-brand' to='/'>
+          Verkkokauppa
+        </Link>
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarNavAltMarkup'
+          aria-controls='navbarNavAltMarkup'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+          <div className='navbar-nav'>
+            <Link className='nav-link' to='/'>
+              Etusivu
+            </Link>
+            <div className='nav-item dropdown'>
+              <Link
+                className='nav-link dropdown-toggle'
+                to='/'
+                id='navbarDropdownMenuLink'
+                role='button'
+                data-bs-toggle='dropdown'
+                aria-expanded='false'
+              >
+                Tuotteet
+              </Link>
+              <ul
+                className='dropdown-menu'
+                aria-labelledby='navbarDropdownMenuLink'
+              >
+                <li>
+                  <Link className='dropdown-item' to='/'>
+                    Tuoteryhmä 1
+                  </Link>
+                </li>
+                <li>
+                  <Link className='dropdown-item' to='/'>
+                    Tuoteryhmä 2
+                  </Link>
+                </li>
+                <li>
+                  <Link className='dropdown-item' to='/'>
+                    Tuoteryhmä 3
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <Link className='nav-link' to='/'>
+                Tietoa meistä
+              </Link>
+            </div>
+            <form className="d-flex align-self-end">
+              <input className="form-control me-2" type="search" placeholder="Hae tuotetta nimellä" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Hae</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-</nav>
-    );
+    </nav>
+  )
 }
