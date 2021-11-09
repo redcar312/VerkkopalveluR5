3cc4a6c57fd2d0227f2c91ee9ff031b68d4a4a2e
Joonas Kelahaara <jolle.kela@gmail.com>
2021-11-09 21:45:22 +0200

    vähän muokkausta

--- verkkopalveluprojekti/src/NavBar.js
+++ verkkopalveluprojekti/src/NavBar.js
@@ -63,7 +63,7 @@ export default function NavBar() {
             </div>
             <form className="d-flex">
               <input className="form-control me-2" type="search" placeholder="Hae tuotetta nimellä" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Hae</button>
+              <button className="btn btn-outline-secondary" type="submit">Hae</button>
             </form>
             <div>
               <Link className='nav-link' to='/'>
