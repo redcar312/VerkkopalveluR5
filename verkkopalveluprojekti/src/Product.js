004e69094934e47e225a8804e447062a2c7768d3
Joonasjuhosanteri <81249550+Joonasjuhosanteri@users.noreply.github.com>
2021-11-24 18:47:47 +0200

    vähän pasaa tyylihommaa tuotesivulle (ei lopullinen)

--- verkkopalveluprojekti/src/Product.js
+++ verkkopalveluprojekti/src/Product.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import './Product.css';
 
 
-export default function Product({product, url, addToCart}) {
+export default function Product({ product, url, addToCart }) {
     return (
-        <div className="container-fluid">
+        <div className="container">
             <div className="col-12">
-            <p>{product?.name}</p>
-            <div>
-                  <img src={url + 'img/' + product?.image} alt="" />
-            </div>
-            <p>{product?.price}€</p>
-            <button className="btn btn-warning addbutton" type="button" onClick={e => addToCart(product)}>Lisää</button>
+                <h2>{product?.name}</h2>
+                <div class="kuva">
+                    <img src={url + 'img/' + product?.image} alt="" />
+                </div>
+                <p className='infoTeksti'>{product?.price}€</p>
+                <p className='infoTeksti'>Tähän tulee sitten hullua dataa kaikista usko_mattomista tuotteista</p>
+                <div class="kuva">
+                    <button className="btn btn-warning addbutton" type="button" onClick={e => addToCart(product)}>Lisää Ostoskoriin</button>
+                </div>
             </div>
         </div>
     )
