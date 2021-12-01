5963d202dbf4e59686bba09dae6f3b74e92358a3
Joonasjuhosanteri <81249550+Joonasjuhosanteri@users.noreply.github.com>
2021-12-01 18:59:36 +0200

    vähän jotain about ussiin ja footteriin linkki joka vie sivulle jossa lukee maksutavoista

--- verkkopalveluprojekti/src/AboutUs.js
+++ verkkopalveluprojekti/src/AboutUs.js
@@ -3,9 +3,19 @@ import './App.css';
 
 export default function AboutUs() {
     return (
-        <div className="container-fluid">
+        <div className="container">
+            <h2>Tietoa meistä</h2>
             <div className="col-12">
-                Tällä sivulla on tietoa meistä
+                *placeholderi nimelle* on vuonna 2021 perustettu verkkokauppa, joka on tehty Verkkopalveluprojektia varten. Myymälässä myydään kaikkea mahdollista perus elektroniikkaa
+                puhelimista kodinkoneisiin. Tekijöinä ovat OAMKin opiskelijat Joonas Kelahaara, Konsta Jäske, Riku Honkamäki, Matias Hurtamo ja Joonas Kokko. Sivusto on toteutettu käyttämällä REACTia, PHPtä ja SQLlää.
+                Jos joku keksii jotain hyviä lisätietoja tänne niin lisäilkää menemään, mulla ei mielikuvitus riitä
+        
+            <h3 class="tietoja">
+                Maksutavat
+            </h3>
+            <div class="infoTeksti">
+                Maksutapoina yrityksessämme toimii Maksukortti, tilisiirto ja maksuluotto. Tuotteet on mahdollista saada myös laskulla.
+            </div>
             </div>
         </div>
     )
--- verkkopalveluprojekti/src/inc/Footer.js
+++ verkkopalveluprojekti/src/inc/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return(
@@ -6,7 +7,12 @@ export default function Footer() {
                 <div className="container-fluid d-flex justify-content-between">
                     <h6 className="ms-auto col-lg-4 col-md-3 col-sm-12">Info</h6>
                     <h6 className="ms-auto col-lg-4 col-md-3 col-sm-12">Verkkokauppa</h6>
-                    <span className="justify-content-end ms-auto col-lg-4 col-md-3 col-sm-12">&copy; Ryhmä 5: Konsta Jäske, Joonas Kelahaara, Joonas Kokko, Matias Hurtamo, Riku Honkamäki</span>
+                    <span className="ms-auto col-lg-4 col-md-3 col-sm-12">&copy;Konsta Jäske, Joonas Kelahaara, Joonas Kokko, Matias Hurtamo, Riku Honkamäki</span>
+                </div>
+                <div className="container-fluid d-flex justify-content-between">
+                <Link className='link' to='/aboutus'>
+                Maksutavat
+                </Link>
                 </div>
             </footer>
     )
