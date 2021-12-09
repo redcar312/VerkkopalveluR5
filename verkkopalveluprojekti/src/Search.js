2f83b6b59be854a55865cb33caf856d8df4bc5b3
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-09 18:03:36 +0200

    hakupalkki toimii ja vähän css juttuja

--- verkkopalveluprojekti/src/Search.js
+++ verkkopalveluprojekti/src/Search.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
+import './App.css';
 
 export default function Search({url, search}) {
     const[products, setProducts] = useState([]);
@@ -18,16 +20,37 @@ export default function Search({url, search}) {
                     alert(error.response.data.error);
                   }
         })
-    })
+    }, [search])
 
     return (
         <div className="container">
+            <h2>Haulla "{search}" löytyi:</h2>
             {products.map(product => (
                 <ul>
-                    <li key={product.id}>
-                        {product.name}, 
-                        {product.price} €, 
-                        <img src={url + 'img/' + product.image} alt="" />
+                    <li className="searchProduct" key={product.id}>
+                        <Link className="productLink"
+                            to={{
+                            pathname: '/product',
+                            state: {
+                            id: product.id,
+                            name: product.name,
+                            price: product.price,
+                            image: product.image
+                            }
+                            }}>{product.name}
+                        </Link>
+                        <p>{product.price} €</p>
+                        <Link to={{
+                            pathname: '/product',
+                            state: {
+                                id: product.id,
+                                name: product.name,
+                                price: product.price,
+                                image: product.image
+                                }
+                            }}>
+                        <img className="img-fluid searchImage" src={url + 'img/' + product.image} alt="" />
+                        </Link>
                     </li>
                 </ul>
             ))}
