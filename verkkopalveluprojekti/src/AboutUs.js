028bc56f45e2f520f234b51de3254cde77ec6fdc
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-14 19:51:15 +0200

    tekstien keskitystä js css

--- verkkopalveluprojekti/src/AboutUs.js
+++ verkkopalveluprojekti/src/AboutUs.js
@@ -8,7 +8,6 @@ export default function AboutUs() {
             <div className="col-12 tietoja">
                 Voima on vuonna 2021 perustettu verkkokauppa, joka on tehty Verkkopalveluprojektia varten. Myymälässä myydään kaikkea mahdollista perus elektroniikkaa
                 puhelimista kodinkoneisiin. Tekijöinä ovat OAMKin opiskelijat Joonas Kelahaara, Konsta Jäske, Riku Honkamäki, Matias Hurtamo ja Joonas Kokko. Sivusto on toteutettu käyttämällä REACTia, PHPtä ja SQLlää.
-                Jos joku keksii jotain hyviä lisätietoja tänne niin lisäilkää menemään, mulla ei mielikuvitus riitä
             <h2 className="tietoja">
                 Maksutavat
             </h2>
