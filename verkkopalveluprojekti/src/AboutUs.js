3b629ca608bfc3c9580adfdf93719c323e584b8f
Joonasjuhosanteri <81249550+Joonasjuhosanteri@users.noreply.github.com>
2021-12-14 17:08:39 +0200

    tietoja- ja tuotesivua

--- verkkopalveluprojekti/src/AboutUs.js
+++ verkkopalveluprojekti/src/AboutUs.js
@@ -5,7 +5,7 @@ export default function AboutUs() {
     return (
         <div className="container">
             <h2>Tietoa meistä</h2>
-            <div className="col-12">
+            <div className="col-12 tietoja">
                 *placeholderi nimelle* on vuonna 2021 perustettu verkkokauppa, joka on tehty Verkkopalveluprojektia varten. Myymälässä myydään kaikkea mahdollista perus elektroniikkaa
                 puhelimista kodinkoneisiin. Tekijöinä ovat OAMKin opiskelijat Joonas Kelahaara, Konsta Jäske, Riku Honkamäki, Matias Hurtamo ja Joonas Kokko. Sivusto on toteutettu käyttämällä REACTia, PHPtä ja SQLlää.
                 Jos joku keksii jotain hyviä lisätietoja tänne niin lisäilkää menemään, mulla ei mielikuvitus riitä
