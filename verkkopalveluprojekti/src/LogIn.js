8258f9a75e89e3ad051ee0d7cbc690bb1f3d8033
redcar312 <67953889+redcar312@users.noreply.github.com>
2021-12-16 22:14:34 +0200

    Update LogIn.js

--- verkkopalveluprojekti/src/LogIn.js
+++ verkkopalveluprojekti/src/LogIn.js
@@ -42,19 +42,7 @@ export default function LogIn() {
               
               </form>  
          
-        <div className='container'>
-            <h1>Kirjaudu sisään</h1>
-            <h4>Käyttäjänimi</h4>
-                <input id="dd" type="text" />
-            <h4>Salasana</h4>
-                <input id="dd" type="text" />
-            <div className="row">
-                <button className="btn btn-warning">Kirjaudu</button>
-            </div>
-            <div className="row">
-                <Link to="/register">Etkö ole vielä asiakas? Rekisteröidy tästä!</Link>
-            </div>
-
-        </div>
+     
+      
     )
 }
