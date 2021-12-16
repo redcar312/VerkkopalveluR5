106a63f16f0d1120a05a24dec216291c948d6e5c
RikuH <n0hori00@studemts.oamk.fi>
2021-12-16 18:18:56 +0200

    login ulkoasu ready

--- verkkopalveluprojekti/src/LogIn.js
+++ verkkopalveluprojekti/src/LogIn.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom'
 
 export default function LogIn() {
     return (
-        <div>
+        <div className='container'>
             <h1>Kirjaudu sisään</h1>
-            <h3>Käyttäjänimi</h3>
+            <h4>Käyttäjänimi</h4>
             <input id="dd" type="text" />
-            <h3>Salasana</h3>
+            <h4>Salasana</h4>
             <input id="dd" type="text" />
+            <div class="row">
             <Link to="/register">Etkö ole vielä asiakas? Rekisteröidy tästä!</Link>
+            </div>
         </div>
     )
 }
