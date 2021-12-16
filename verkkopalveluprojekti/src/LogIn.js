8b215804d5bf98d60ca3db725f37fd41d5ebb44f
Matias Hurtamo <n0huma00@students.oamk.fi>
2021-12-16 22:04:32 +0200

    asd

--- verkkopalveluprojekti/src/LogIn.js
+++ verkkopalveluprojekti/src/LogIn.js
@@ -1,16 +1,47 @@
 import React from 'react'
 import './LogIn.css';
 import { Link } from 'react-router-dom'
-
+import { useState } from 'react';
 export default function LogIn() {
+ const URL = "http://localhost/verkkopalveluprojekti_ryhma_5/login/login.php"   
+ const [username, setUsername] = useState('')
+ const [passwd, setPasswd] = useState ('')  
+    function checkUser(e){
+    e.preventDefault();
+    fetch(URL,{
+        method:'POST',
+        headers: { 
+          'Content-Type' : 'application/json',
+          
+        },
+        body: JSON.stringify({
+            username: username,
+            passwd: passwd
+          })
+           })
+          .then (res=> {
+            return res.json
+          }). catch(error => alert(error))
+      
+    }
+    
     return (
         <div>
-            <h1>Kirjaudu sisään</h1>
-            <h3>Käyttäjänimi</h3>
-            <input id="dd" type="text" />
-            <h3>Salasana</h3>
-            <input id="dd" type="text" />
-            <Link to="/register">Etkö ole vielä asiakas? Rekisteröidy tästä!</Link>
+          <form onSubmit={checkUser}>
+           <div>
+            <label>Käyttäjätunnus</label>
+            <input value={username} onChange={e => setUsername(e.target.value)}></input>
+            </div>   
+            <div>
+                <label>Salasana</label>
+                <input value={passwd} onChange={e => setPasswd(e.target.value)}></input>
+            </div>
+             <div>
+             <Link to="/register">Etkö ole vielä asiakas? Rekisteröidy tästä!</Link>
+            </div> 
+              
+              </form>  
+          
         </div>
     )
 }
