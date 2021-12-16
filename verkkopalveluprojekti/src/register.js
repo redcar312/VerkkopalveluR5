e2b6743df7a22765f11565962b0371a414b4eb86
Joonas Kelahaara <jolle.kela@gmail.com>
2021-12-16 21:52:18 +0200

    paskaa

--- verkkopalveluprojekti/src/register.js
+++ verkkopalveluprojekti/src/register.js
@@ -30,19 +30,7 @@ export default function Register () {
     .then (res=> {
       return res.json
     }). catch(error => alert(error))
-
-    
-
-   
   }
-
-  
-    
-  
-      
-  
-    
-
 
   return (
     <div className="container">
